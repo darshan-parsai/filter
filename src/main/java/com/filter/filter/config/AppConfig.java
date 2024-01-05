9a783825345bcf49527af7af54c52f55f69a2345
darshan-parsai <darshanparsai@gmail.com>
2024-01-05 14:15:56 +0530

    added spring security

---
+++ src/main/java/com/filter/filter/config/AppConfig.java
@@ -0,0 +1,33 @@
+package com.filter.filter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails userDetails = User.builder().username(" ").password(passwordEncoder().encode("abc")).roles("ADMIN")
+                .build();
+        return  new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
---
+++ src/main/java/com/filter/filter/model/JwtResponse.java
@@ -0,0 +1,15 @@
+package com.filter.filter.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+   private String jwtToken;
+   private String userName;
+}
